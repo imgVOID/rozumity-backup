de2d0e1087442ba072f8855df47bb02d4c37647a
imgvoid <imgvoid@gmail.com>
2023-05-04 19:58:27 +0300

    fix jsonapi serialization

--- rozumity/accomplishments/serializers.py
+++ rozumity/accomplishments/serializers.py
@@ -1,10 +1,14 @@
 
 from rozumity.serializers import JSONAPI
-
+from cities_light.models import Country
 from .models import University
-    
+
 
 class UniversityJSONAPI(JSONAPI):
     class Meta:
         model = University
-        fields = ['id', 'title']
+
+
+class CountryJSONAPI(JSONAPI):
+    class Meta:
+        model = Country
--- rozumity/rozumity/serializers.py
+++ rozumity/rozumity/serializers.py
@@ -1,21 +1,37 @@
 from rest_framework import serializers
 from django.forms.models import model_to_dict
+from django.db import models
 
 
-class BaseJSONAPI(serializers.BaseSerializer):
-    def get_jsonapi_body(self, instance):
-        fields = model_to_dict(instance)
-        del fields['id']
-        return {
-            'type': instance.__class__.__name__.lower(),
-            'id': instance.id,
+class BaseJSONAPI(serializers.ModelSerializer):
+    def get_jsonapi_body(self, type, instance_id, fields, relations={}):
+        relations = {k: {'data': {'id': v, 'type': k}} for (k, v) in relations.items()}
+        data = {
+            'type': type,
+            'id': instance_id,
             'attributes': fields,
-            'relations': {}
+            'relationships': relations
         }
+        if not data['relationships']:
+            del data['relationships']
+        return data
     
     def to_representation(self, instance):
-        return {k: v for (k, v) in 
-                self.get_jsonapi_body(instance).items() if v}
+        fields = [
+            f for f in instance._meta.get_fields() 
+            if not f.auto_created and f.concrete
+        ]
+        relations = {
+            str(f).split('.')[-1]: f.value_from_object(instance) 
+            for f in fields if f.related_model
+        }
+        fields = {
+            str(f).split('.')[-1]: f.value_from_object(instance) 
+            for f in fields if not f.related_model
+        }
+        return self.get_jsonapi_body(
+            instance.__class__.__name__.lower(), instance.id, fields, relations
+        )
 
 
 class JSONAPI(BaseJSONAPI):
